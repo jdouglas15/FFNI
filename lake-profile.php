f2c6d861ee0c94c05c62b19e87f0a0eef9d292fb
jdouglas15 <jdouglas199610@gmail.com>
2021-06-25 19:47:37 +0100

    test

--- lake-profile.php
+++ lake-profile.php
@@ -138,7 +138,7 @@ if (mysqli_num_rows($get_lake_info_result) > 0) {
                                 <div class="row no-gutters align-items-center">
                                     <div class="col mr-2">
                                         <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                            Boats Avaialble? </div>
+                                            Are Boats Avaialble? </div>
                                         <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $lake_boat_option ?></div>
                                     </div>
                                     <div class="col-auto">
